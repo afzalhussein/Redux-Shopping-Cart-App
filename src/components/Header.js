98f0f98893e16f878b5dfa471de46be3198f0aef
Syed Afzal Hussain <afzalhussein@hotmail.com>
2024-10-24 23:13:14 -0400

    Refactored CSS for better UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Cart from "./Cart";
 import "./Header.css";
 const Header = () => {
+  const options = useCallback( options );
   return (
     <header>
       <nav className="header-nav">
-        <LiContainer options={options()} />
+        <LiContainer options={options({ appName: "Redux Shopping App" })} />
       </nav>
     </header>
   );
 };
 
 export default Header;
-
-function options() {
+/**
+ * This return an array of Element
+ * @todo: Need to find a way to pass props to individual elements then what happens? Refactor to break up or some other mechanism?
+ * @returns Element[]
+ */
+function options ( props ) {
+  console.log( props.appName );
   return [
     <h2
       className="header-h2"
       style={{ fontFamily: "monospace", fontSize: "30px" }}
     >
-      Redux Shopping App
+      {props.appName}
     </h2>,
     <Cart />,
   ];
